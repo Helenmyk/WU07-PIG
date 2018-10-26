7036eb70a84ebc8512d68c1e6da083778e126e31
Martypartyy <42603131+Martypartyy@users.noreply.github.com>
2018-10-26 14:52:39 +0200

    test

---
+++ hangman/alfa.js
@@ -0,0 +1,164 @@
+/*------------- constants -------------*/
+var ord = [
+    'HØNEMOR',
+    'LESEHEST',
+    'RINGREV',
+    'DANSELØVE',
+    'HERMEGÅS',
+    'SNEGLEFART',
+    'LINSELUS',
+    'LÅRHØNE',
+    'SVINDYRT',
+    'TULLEBUKK',
+    'VINGLEMUS',
+    'DUMPAPP',
+    'GRISEVÆR',
+    'FYTTIGRISEN',
+    'GRISEFLAKS',
+    'GRISEGOD',
+    'HELDIGGRIS',
+    'REDDHARE',
+    'KLATREMUS'
+];
+
+/*------------- constants -------------*/
+var utrykk = [
+    'SLU SOM EN REV',
+    'STILLE SOM EN MUS',
+    'DUM SOM EN SAU',
+    'STA SOM ET ESEL',
+    'TJUKK SOM EN GRIS',
+    'SVETTER SOM EN GRIS',
+    'TROFAST SOM EN HUND',
+    'STOLT SOM EN HANE',
+    'FEIG SOM EN KYLLING',
+    'LITEN SOM EN MUS',
+    'SNILL SOM ET LAM',
+    'SMIDIG SOM EN KATT',
+    'KAKLE SOM EN HØNE',
+    'HYLE SOM EN GRIS',
+    'STYGG SOM EN KU',
+    'LEKEN SOM EN KATT',
+    'VAGLE SOM EN GÅS',
+    'KJØRE SOM ET SVIN',
+    'FATTIG SOM EN KIRKEROTTE',
+    'GALE SOM EN HANE',
+    'MJAUE SOM EN KATT',
+    'FROM SOM ET LAM',
+    'SINT SOM EN OKSE',
+    'PIPE SOM EN MUS',
+    'KLATRE SOM EN GEIT',
+    'KLAR SOM ET EGG',
+    'FRI SOM FUGLEN',
+    'LISTE SEG PÅ KATTEPOTER',
+    'KASTE PERLER FOR SVIN',
+    'LOPPER I BLODET',
+    'KLEKKE UT EN PLAN',
+    'DET GÅR DEN VEIEN HØNA SPARKER'
+    'HA SINE SVIN PÅ SKOGEN',
+    'DET ER BEDRE MED EN FUGL I HÅNDA ENN TI PÅ TAKET',
+    'Å REGNE HUNDER OG KATTER',
+    'SULTEN SOM EN HEST',
+    'SKVETTE VANN PÅ GÅSA',
+    'INGEN VET HVOR HAREN HOPPER',
+    'GJØR IKKE EN FLUE FORTRED'
+    'SULTEN SOM EN ULV'
+
+];
+
+/*------------- app's state -------------*/
+var secretWord, wrongCount, guess;
+
+/*------------- cached element references -------------*/
+var $guess = $('#guess');
+var $img = $('#hang-img');
+var $message = $('#message');
+
+/*------------- event listeners -------------*/
+$('#letters').on('click', handleLetterClick);
+
+$('#reset').on('click', initialize);
+
+/*------------- functions -------------*/
+initialize();
+
+function initialize() {
+    wrongCount = 0;
+    secretWord = words[getRandomInt(words.length -1)];
+
+    guess = "";
+
+    for (var i = 0; i < secretWord.length; i++) {
+      var currentLetter = secretWord[i];
+      if (currentLetter === " ") {
+          guess += " "
+      } else {
+          guess += "_";
+      }
+    };
+
+    $('button.letter-button').prop('disabled', false);
+    render();
+}
+
+function getRandomInt(max) {
+    return Math.floor (Math.random() * (max + 1));
+}
+
+function render() {
+    $guess.html(guess);
+      // pop balloong
+    $img.attr('src', 'images/img' + wrongCount + '.png')
+
+    if (guess === secretWord) {
+        // Hvis vinner
+    } else if ( wrongCount === 6) {
+        // Hvis Taper
+    } else {
+        // ?
+    }
+}
+
+
+//skrur av lyden på siden
+function pauseLyd()
+{
+  var lyd = document.getElementById("lydfil");//setter en variabel til lydfilen som kjøres
+  if(lyd.muted==true)//sjekker om lyden er skrudd av
+  {
+    lyd.muted=false;//skrur på lyden
+    stille.src="../Bilder/Speaker.png";//gir bilde til lyd-knappen
+  }
+  else
+  {
+    lyd.muted=true;//skrur av lyden
+    stille.src="../Bilder/no-sound.png";//gir bilde til lyd-knappen
+  }
+  stille.blur();//fjerner fokus fra knappen så den ikke skrus av/på ved trykk på enter
+}
+
+
+function handleLetterClick (evt) {
+    if (wrongCount === 6) return;
+    evt.target.style.backgroundColor = "white";
+    evt.target.style.opacity = "0.5";
+    var letter = evt.target.textContent;
+    console.log(secretWord);
+    if (secretWord.includes(letter)) {
+        var pos = secretWord.indexOf(letter);
+        while ( pos >= 0) {
+            guess = guess.split('');
+            guess[pos] = letter;
+            guess = guess.join('');
+            pos = secretWord.indexOf(letter,pos +1);
+        }
+    } else {
+        if (evt.target.id !== "reset") {
+            wrongCount++;
+        }
+    }
+
+    $(evt.target).prop('disabled', true);
+    $('#reset').prop('disabled', false);
+    render();
+}
