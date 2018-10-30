239ac83a76f1a8854fb89aa7f0e1410cb4a5f2f8
marilhan <marius-leite@hotmail.com>
2018-10-30 12:57:49 +0100

    ?

--- hangman/Game/lett/alfa.js
+++ hangman/Game/lett/alfa.js
@@ -13,27 +13,7 @@ window.addEventListener("resize", function() {
     }
   }
 });
-<<<<<<< HEAD
 /*(let player;
-=======
-
-//Funskjon som skrur bakgrunnsmusikken av på siden
-function ingenLyd() {
-  var lyd = document.getElementById("lydFil");
-    if(lyd.muted==true) //Sjekker om lyden er av
-     {
-      lyd.muted=false; //Skrur på lyden dersom den er av
-      lydBilde.src="../Bilder/speaker.png"; //Gir et bilde til knappen
-  } else {
-          lyd.muted=true; //Hvis lyden er på, skrus lyden av
-          lydBilde.src="../Bilder/no-sound.png"; //Gir et bilde til knappen
-        }
-        lydBilde.blur(); //Fjerner fokus fra knappen så den ikke skrus av/på ved trykk på enter
-      }
-
-
-let player;
->>>>>>> 822332d0b8b148ece57e29b8769611f79012ad30
 let isChrome =
   /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 if (!isChrome) {
