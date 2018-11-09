726e011b55d15e99ab955f3f6ef6af16c4b8fcaf
marilhan <marius-leite@hotmail.com>
2018-11-09 10:17:06 +0100

    :O

--- hangman/Game/forside/forside.js
+++ hangman/Game/forside/forside.js
@@ -1,12 +1,23 @@
+let vgrad = document.getElementById("vGrad");
 function startSpill() {
-  var vgrad = document.getElementById("vGrad");
+  vgrad = document.getElementById("vGrad");
   vgrad.style.animation = "testAnimation 0.7s ease-out 0s normal 1 forwards";
+  if (menyBoksHistorie.style.display == "block") {
+    menyBoksHistorie.style.display = "none";
+  } else if (menyBoksRegler.style.display == "block") {
+    menyBoksRegler.style.display = "none";
+  }
+  document.getElementById("regler").disabled = true;
+  document.getElementById("historie").disabled = true;
 }
 
 function tilbakeVGrad() {
-  var vgrad = document.getElementById("vGrad");
+  vgrad = document.getElementById("vGrad");
   vgrad.style.top = "100px";
   vgrad.style.animation = "tilbakeAnimasjon 0.7s ease-in 0s normal 1 forwards";
+
+  document.getElementById("regler").disabled = false;
+  document.getElementById("historie").disabled = false;
 }
 
 function regler() {
